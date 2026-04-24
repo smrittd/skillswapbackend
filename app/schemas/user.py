3c47b74f9cf003db5770213ed5ea75d30f098120
Alisher <alexkarabayev@gmail.com>
2026-04-24 20:56:14 +0500

    feat: add pydantic schemas for user

---
+++ app/schemas/user.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, EmailStr, ConfigDict, Field
+
+class UserBase(BaseModel):
+    email: EmailStr
+    usernaname: str
+
+class UserCreate(UserBase):
+    password: str
+
+class UserResponce(UserBase):
+    id: int
+    is_active: bool
+
+
+    model_config = ConfigDict(from_attributes=True)
